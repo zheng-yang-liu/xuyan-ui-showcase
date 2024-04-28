66ad0c49fa81314f929f65b54dccddec3a8f764d
zheng-yang-liu <thezhengyang@qq.com>
2024-04-28 10:29:47 +0800

    优化

---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    niao
+  </div>
+</template>
+
+<script setup lang="ts">
+import {reactive} from 'vue'
+
+const props = defineProps({})
+const state = reactive({})
+</script>
+
+<style lang="scss">
+
+</style>
