8b1003428cbb9b443b1b50153de842cdd0aa9257
zheng-yang-liu <thezhengyang@qq.com>
2024-04-28 10:27:28 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server:{
+    host:"10.3.197.142",
+    port:8999,
+  },
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
